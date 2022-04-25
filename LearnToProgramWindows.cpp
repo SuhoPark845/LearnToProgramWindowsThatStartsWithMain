6136df8a275a4fade002b7b8f763fb1ef5fdf924
Suho Park <psooh1219@naver.com>
2022-04-25 23:21:05 +0900

    초기 커밋.

---
+++ LearnToProgramWindows.cpp
@@ -0,0 +1,137 @@
+﻿// LearnToProgramWindows.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+#ifndef UNICODE
+#define UNICODE
+#endif 
+
+#include <windows.h>
+#include <iostream>
+
+// Define a structure to hold some state information.
+struct StateInfo {
+    // ... (struct members not shown)
+};
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+inline StateInfo* GetAppState(HWND hwnd);
+void OnSize(HWND hwnd, UINT flag, int width, int height);
+
+int main()
+{
+    // Register the window class.
+    const wchar_t CLASS_NAME[] = L"Sample Window Class";
+    StateInfo* pState = new (std::nothrow) StateInfo;
+
+    if (pState == NULL)
+    {
+        return 0;
+    }
+
+    WNDCLASS wc = { };
+
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = GetModuleHandle(NULL);
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    // Create the window.
+
+    HWND hwnd = CreateWindowEx(
+        0,                              // Optional window styles.
+        CLASS_NAME,                     // Window class
+        L"Learn to Program Windows",    // Window text
+        WS_OVERLAPPEDWINDOW,            // Window style
+
+        // Size and position
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+        NULL,           // Parent window    
+        NULL,           // Menu
+        wc.hInstance,   // Instance handle
+        pState          // Additional application data
+    );
+
+    if (hwnd == NULL)
+    {
+        return 0;
+    }
+
+    ShowWindow(hwnd, SW_SHOWNORMAL);
+
+    MSG msg = { };
+    while (GetMessage(&msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    StateInfo* pState;
+    if (uMsg == WM_CREATE)
+    {
+        CREATESTRUCT* pCreate = reinterpret_cast<CREATESTRUCT*>(lParam);
+        pState = reinterpret_cast<StateInfo*>(pCreate->lpCreateParams);
+        SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)pState);
+    }
+    else
+    {
+        pState = GetAppState(hwnd);
+    }
+
+	switch (uMsg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+
+	case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+
+        // All painting occurs here, between BeginPaint and EndPaint.
+
+        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+
+        EndPaint(hwnd, &ps);
+    }
+		return 0;
+
+	case WM_SIZE:
+    {
+        int width = LOWORD(lParam);  // Macro to get the low-order word.
+        int height = HIWORD(lParam); // Macro to get the high-order word.
+
+        // Respond to the message:
+        OnSize(hwnd, (UINT)wParam, width, height);
+    }
+		break;
+
+	case WM_CLOSE:
+		if (MessageBox(hwnd, L"Really quit?", L"My application", MB_OKCANCEL) == IDOK)
+		{
+			DestroyWindow(hwnd);
+		}
+        // Else: User canceled. Do nothing.
+		return 0;
+
+	}
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+inline StateInfo* GetAppState(HWND hwnd)
+{
+    LONG_PTR ptr = GetWindowLongPtr(hwnd, GWLP_USERDATA);
+    StateInfo* pState = reinterpret_cast<StateInfo*>(ptr);
+    return pState;
+}
+
+void OnSize(HWND hwnd, UINT flag, int width, int height)
+{
+    // Handle resizing
+}
